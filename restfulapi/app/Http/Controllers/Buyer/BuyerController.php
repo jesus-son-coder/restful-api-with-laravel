695e08dfb4279c6afb61aaa91fd824eb2890095b
hseka <hseka@lescollectionneurs.com>
2021-10-16 05:29:19 +0200

    58. Implementing The Show Method for BuyerController Le commit précédent concerne en réalité le : "57. Implementing The Index Method for BuyerController"

--- restfulapi/app/Http/Controllers/Buyer/BuyerController.php
+++ restfulapi/app/Http/Controllers/Buyer/BuyerController.php
@@ -20,26 +20,6 @@ class BuyerController extends Controller
         return response()->json(['message' => 'Liste of all Buyers', 'data' => $buyers]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -49,40 +29,10 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $buyer], 200);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
