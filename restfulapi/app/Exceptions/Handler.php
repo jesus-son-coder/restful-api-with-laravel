96f1624ff36dd2d70129cb517ebbb51b134585ba
hseka <hseka@lescollectionneurs.com>
2021-10-16 10:28:30 +0200

    71. Gérer toutes les autres erreurs de type "HttpException"

--- restfulapi/app/Exceptions/Handler.php
+++ restfulapi/app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Throwable;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -76,6 +77,10 @@ class Handler extends ExceptionHandler
 
         if($exception instanceof NotFoundHttpException) {
             return $this->errorResponse('The specified URL cannot be found', 404);
+        }
+
+        if($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         }
 
         return parent::render($request, $exception);
