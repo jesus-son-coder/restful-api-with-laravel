c436b72b00b439f47ae71d3094ed5b74f379a029
hseka <hseka@lescollectionneurs.com>
2021-10-16 15:25:41 +0200

    78. Utiliser les "Soft Deleting" pour tous les Models

--- restfulapi/app/Models/Product.php
+++ restfulapi/app/Models/Product.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     const UNAVAILABLE_PRODUCT = 'unavailable';
     const AVAILABLE_PRODUCT = 'available';
