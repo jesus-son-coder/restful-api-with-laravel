d16d6471faffeeaa024e65deee1ec2de70945c48
hseka <hseka@lescollectionneurs.com>
2021-10-16 14:46:01 +0200

    75. Retrouver un Buyer à l'aide des Scope Globaux de Laravel (Resolving Buyer Using Laravel Global Scopes)

--- restfulapi/app/Models/Buyer.php
+++ restfulapi/app/Models/Buyer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Transaction;
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,6 +13,12 @@ class Buyer extends User
     use HasFactory;
 
     protected $table = "users";
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
 
     public function transactions()
     {
