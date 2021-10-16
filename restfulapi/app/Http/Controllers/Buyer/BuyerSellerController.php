43acadece62c5acc510b98fe2cba346166872d71
hseka <hseka@lescollectionneurs.com>
2021-10-16 22:41:55 +0200

    92. Fix on "unique"

--- restfulapi/app/Http/Controllers/Buyer/BuyerSellerController.php
+++ restfulapi/app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -19,7 +19,8 @@ class BuyerSellerController extends ApiController
         $sellers =  $buyer->transactions()->with('product.seller')
                     ->get()
                     ->pluck('product.seller')
-                    ->unique();
+                    ->unique('id')
+                    ->values();
 
         return $this->showAll($sellers, "List of Sellers of this Buyer");
     }
