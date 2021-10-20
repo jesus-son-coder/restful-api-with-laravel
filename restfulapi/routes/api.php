2b3a69a9404ce059ffc96f8238ca138bfff62a09
hseka <hseka@lescollectionneurs.com>
2021-10-20 21:02:15 +0200

    108. Implémenter la méthode "Index" de "ProductCategoryController" -- Lister les Category d'un Product

--- restfulapi/routes/api.php
+++ restfulapi/routes/api.php
@@ -51,6 +51,7 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
  */
 Route::resource('products', 'Product\ProductController', ['only' => ['index','show']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['except' => ['create','show','edit']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 
 /**
