d7578ad3ff7c2770b9edf109727483318ab8e872
hseka <hseka@lescollectionneurs.com>
2021-10-20 11:08:35 +0200

    97. Implémenter la méthode "Index" du contrôleur "CategoryBuyerController" -- Lister les Buyers d'une Category

---
+++ restfulapi/app/Http/Controllers/Category/CategoryBuyerController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryBuyerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        $buyers = $category->products()
+            ->whereHas('transactions')
+            ->with('transactions.buyer')
+            ->get()
+            ->pluck('transactions')
+            ->collapse()
+            ->pluck('buyer')
+            ->unique('id')
+            ->values()
+        ;
+
+        return $this->showAll($buyers,
+            'Lits of all the Buyers of this Category',
+            200
+        );
+    }
+
+
+
+
+}
